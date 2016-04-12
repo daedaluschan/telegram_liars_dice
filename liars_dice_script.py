ca61700a1fe83666c640e3bb990a04a2646d7cd0
Daedalus Chan <daedalus.chan@gmail.com>
2016-04-13 00:06:35 +0800

    inviting flow

--- liars_dice_script.py
+++ liars_dice_script.py
@@ -1,4 +1,4 @@
 # coding=UTF8
 
 bot_starting_script = u'你好，如果想玩大話骰，請用 /newgame 。'
-bot_invite_player = u'%s：有 d 悶，不如玩下大話股。\n請先用下面條 URL 開一個同 %s 的 private chat，然後按 start。\n\n%s'
+bot_invite_player = u'%s：有 d 悶，不如玩下大話股。\n請先用下面條 URL 開一個同 %s 的 private chat，然後按 start 報名。\n\n%s'
